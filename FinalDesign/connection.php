cde39cf7c894c6112ee0ae6c4d98ee57a95a41cd
Noor Ahmad <noorahmad1990@gmail.com>
2019-01-20 15:25:03 +0500

    database connection php

---
+++ FinalDesign/connection.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noor Ahmad Zafar
+ * Date: 20-Jan-19
+ * Time: 3:18 PM
+ */
+
+$con = mysqli_connect("localhost","root","","cookeatsleep");
+if(!$con)
+    die("Connection failed");
+
+
