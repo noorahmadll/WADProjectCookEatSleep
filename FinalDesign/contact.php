6c446230d7574a67d129fab899527c70d3f320d8
Noor Ahmad <noorahmad1990@gmail.com>
2019-01-20 12:02:48 +0500

    contact php

---
+++ FinalDesign/contact.php
@@ -0,0 +1,89 @@
+<?php
+    require "Templates.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1.0">
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>CookEatSleep - Contact</title>
+    <link rel="stylesheet" href="css/allinone.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+</head>
+<body class="">
+
+<?php
+	Show_Navbar();
+?>
+
+<br><br>
+<div class="h-100">
+    <div class="d-flex justify-content-center h-100">
+        <div class="sb">
+            <input class="search_input" type="text" name="" placeholder="FIND ANY FOOD">
+            <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
+        </div>
+    </div>
+</div>
+<br><br>
+
+<div class="container-fluid hoverable" style="">
+    <div class="row p-2">
+        <div class="mx-auto col-lg-7 shadow-lg shadow-md shadow-sm bg-warning">
+            <h1 class="my-4">MEET OUR MEMEBRS</h1><hr>
+            <h4 class="text-justify p-3 text-center"> BLOCK-A MAIN BOULEVARD <br> PHASE 2
+                <br> PCSIR HOSUING SOCIETY <br> LAHORE, PAKISTAN</h4>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid hoverable" style="">
+    <div class="row p-2">
+        <div class="mx-auto col-lg-7 shadow-lg shadow-md shadow-sm bg-warning">
+            <br>
+            <div class="row text-center">
+                <div class="col-sm-3 col-xs-6 p-2">
+                    <h1><span class="fas fa-phone" style="color: red"></span></h1>
+                    <h3>Phone</h3>
+                    <p>+92 315 0722892</p><br>
+                </div>
+                <div class="col-sm-3 col-xs-6 p-2">
+                    <h1><span class="fas fa-home" style="color: red"></span></h1>
+                    <h3>Location</h3>
+                    <p>A-BLOCK PCSIR2</p><br>
+                </div>
+                <div class="col-sm-3 col-xs-6 p-2">
+                    <h1><span class="fas fa-at" style="color: red"></span></h1>
+                    <h3>E-mail</h3>
+                    <p>cookeatsleep@gmail.com</p><br>
+                </div>
+                <div class="col-sm-3 col-xs-6 p-2">
+                    <h1><span class="fas fa-globe" style="color: red"></span></h1>
+                    <h3>Web</h3>
+                    <p>www.cookeatsleep.com</p><br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="page-footer font-small indigo d-none d-lg-block d-xl-block ">
+	<?php Show_Footer(); ?>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<script src="js/index.js"></script>
+</body>
+</html>
