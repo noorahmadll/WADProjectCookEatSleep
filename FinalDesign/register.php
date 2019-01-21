dce9bd35b10f51681e3d1fc313212bac300ba846
Noor Ahmad <noorahmad1990@gmail.com>
2019-01-21 19:52:57 +0500

    registration php

--- FinalDesign/register.php
+++ FinalDesign/register.php
@@ -1,5 +1,65 @@
 <?php
-require "Templates.php"
+require "Templates.php";
+
+if (isset($_POST['register'])) {
+    $fname = $_POST['fname'];
+    $mname = $_POST['mname'];
+    $lname = $_POST['lname'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $pass1 = $_POST['pass1'];
+    $pass2 = $_POST['pass2'];
+    $dob = $_POST['dob'];
+
+    $querryy = "select *from user";
+    $checker = mysqli_query($con, $querryy);
+    while ($row = mysqli_fetch_assoc($checker)) {
+        if ($username === $row['username']) {
+            echo '<script type="text/javascript">alert("Sorry this username is already taken");</script>';
+            exit();
+        } else if ($email === $row['email']) {
+            echo '<script type="text/javascript">alert("Sorry this email is already taken");</script>';
+            exit();
+        }
+    }
+
+
+    $fileType = $_FILES['pro_img']['type'];
+    $fileSize = $_FILES['pro_img']['size'];
+
+    if ($fileSize / 1024 > '2048') {
+        echo '<script type="text/javascript">alert("Sorry too large size");</script>';
+        exit();
+    } //FileSize Checking
+
+    if ($fileType != 'image/png' &&
+        $fileType != 'image/gif' &&
+        $fileType != 'image/jpg' &&
+        $fileType != 'image/jpeg'
+    ) {
+        echo '<script type="text/javascript">alert("Sorry this fomrat image not suported");</script>';
+        exit();
+    } //file type checking ends here.
+    $upFile = './Images/' . date('Y_m_d_H_i_s') . $_FILES['pro_img']['name'];
+
+    if (is_uploaded_file($_FILES['pro_img']['tmp_name'])) {
+        if (!move_uploaded_file($_FILES['pro_img']['tmp_name'], $upFile)) {
+            echo '<script type="text/javascript">alert("error try again later");</script>';
+        } else {
+            //insert query here
+
+            $name = $fname . " " . $mname . " " . $lname;
+            $insert_user = "insert into user (username,DateOfBirth,name,email,password,profilePicture) 
+                  VALUES ('$username','$dob','$name','$email','$pass1','$upFile');";
+            $insert_us = mysqli_query($con, $insert_user);
+            if ($insert_us) {
+                header("location: " . $_SERVER['PHP_SELF']);
+            }
+
+        }
+    }
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,38 +75,52 @@ Show_Navbar();
 <div class="container-fluid bg">
 	<div class="row justify-content-center">
 		<div class="col-auto col-md-5 col-sm-6 col-lg-5 col-xl-4">
-			<form class="form-group form-container bg-warning">
+			<form name="Form" class="form-group form-container bg-warning" onsubmit="return validateForm()"
+				  method="post" enctype="multipart/form-data">
 				<h1 class="text-center"><i class="far fa-smile" style="font-size:150px;color:gray"></i></h1>
 				<hr>
 				<h4 class="text-center"><label>Enter Your Bio:<br></label></h4>
-				<input type="text" class="form-control" id="fname" placeholder="First Name">
+				<input pattern="^[a-zA-z]+$" type="text" class="form-control" name="fname" id="fname"
+					   placeholder="First Name"
+					   required>
 				<br>
-				<input type="text" class="form-control" id="mname" placeholder="Middle Name">
+				<input pattern="^[a-zA-z]+$" type="text" class="form-control" name="mname" id="mname"
+					   placeholder="Middle Name"
+					   required>
 				<br>
-				<input type="text" class="form-control" id="lname" placeholder="Last Name">
+				<input pattern="^[a-zA-z]+$" type="text" class="form-control" name="lname" id="lname"
+					   placeholder="Last Name"
+					   required>
+				<br>
+				<input pattern="^[a-zA-z]+$" type="text" class="form-control" name="username" id="username"
+					   placeholder="Username"
+					   required>
 				<br>
 				<h4 class="text-center"><label>Enter Your Info:<br></label></h4>
-				<input type="email" class="form-control" id="mail" placeholder="Your Email Address">
-				<input type="password" class="form-control" id="pass" placeholder="Your Password">
-				<input type="password" class="form-control" id="passconfirm" placeholder="Your Confirm Password">
+				<input name="email" type="email" class="form-control" id="mail" placeholder="Your Email Address"
+					   required>
+				<input name="pass1" type="password" class="form-control" id="pass" placeholder="Your Password">
+				<input name="pass2" type="password" class="form-control" id="passconfirm"
+					   placeholder="Your Confirm Password" required>
 				<br>
-				<select class="form-control" id="pro_cat" name="pro_cat">
+				<select class="form-control" id="pro_cat" name="pro_cat" required>
 					<option>Your Gender</option>
 					<option>Male</option>
 					<option>Female</option>
 					<option>Transgendered</option>
 				</select>
-				<input type="date" class="form-control" id="dob" placeholder="Date Of Birth">
+				<input name="dob" type="date" class="form-control" id="dob" placeholder="Date Of Birth" required>
 				<br>
-				<input class="form-control" type="file" accept="image/png, image/jpeg" id="pro_img" name="pro_img">
+				<input class="form-control" type="file" accept="image/*" id="pro_img" name="pro_img" required>
 
 				<div class="form-check">
-					<input type="checkbox" class="form-check-input" id="Check1">
+					<input type="checkbox" class="form-check-input" id="Check1" required>
 					<small><label class="form-check-label form-text text-muted text-black-50" for="Check1">I AGREE TO
 							TERMS</label></small>
 				</div>
 				<br>
-				<button type="submit" class="btn btn-primary btn-block w-50 mx-auto">REGISTER NOW</button>
+				<button name="register" type="submit" class="btn btn-primary btn-block w-50 mx-auto">REGISTER NOW
+				</button>
 			</form>
 		</div>
 	</div>
